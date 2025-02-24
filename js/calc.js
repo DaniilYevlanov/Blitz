360a406b2d282a65950e15796c93d41e04ebb0fa
DaniilYevlanov <novelstarter@gmail.com>
2025-02-24 10:49:00 +0200

    sec

---
+++ js/calc.js
@@ -0,0 +1,16 @@
+const counters = document.querySelectorAll(".counter");
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const speed = 100;
+        const increment = target / speed;
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+});
